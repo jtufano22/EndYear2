06b016b9fad7c32b2bcc1cfadcc8db984ad377d7
Joseph Tufano <josephtufano03@gmail.com>
2020-01-06 10:56:16 -0500

    Merge branch 'master' of /Users/jtufano22/IdeaProjects/MidYear2 with conflicts.

--- src/GeometryDash.java
+++ src/GeometryDash.java
@@ -1,9 +1,5 @@
 import javafx.animation.AnimationTimer;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,16 +8,13 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GeometryDash extends Application {
     private ImageView dude = new ImageView("squareDude.png");
-
 
     private int height = 500;
     private int width = 750;
@@ -35,42 +28,33 @@ public class GeometryDash extends Application {
 
     private Rectangle ground = new Rectangle(0, height-68, width+28, 78);
 
-    Pane pane = new Pane();
-    Rectangle r1 = new Rectangle(0,400,30, 30);
-
-
-
-
 
     public void start(Stage stage) {
+        Pane pane = new Pane();
         pane.setStyle("-fx-background-color: radial-gradient(from 100% 100% to 100% 100%, #000000, #FFFFFF);");
 
         ground.setFill(Color.DARKBLUE);
 
-        newBlock();
         dude.relocate(dudeX, dudeY);
-        pane.getChildren().addAll(dude, ground, r1);
-
-
+        pane.getChildren().addAll(dude, ground);
 
         new AnimationTimer() {
             public void handle(long now){
                 pane.setOnKeyPressed(e ->
                 {
                     KeyCode key = e.getCode();
-                    if(key.equals(KeyCode.W) || key.equals(KeyCode.UP) || key.equals(KeyCode.SPACE)) {
+                    if (key.equals(KeyCode.W) || key.equals(KeyCode.UP) || key.equals(KeyCode.SPACE)) {
                         jump = true;
                         up = true;
                         startingPos = dudeY;
                     }
                 });
                 if(jump) {
-                    jump();
+                    ascend();
+                    descend();
                 }
                 dude.relocate(dudeX, dudeY);
                 pane.requestFocus();
-
-
             }
         }.start();
 
@@ -82,14 +66,16 @@ public class GeometryDash extends Application {
         stage.show();
     }
 
-    public void jump() {
-        if(up && startingPos-96 < dudeY) {
-            dudeY-=4;
-        }
-        else if(startingPos-96 >= dudeY) {
+    public void ascend() {
+        if (up && startingPos - 96 < dudeY) {
+            dudeY -= 4;
+        } else if (startingPos - 96 >= dudeY) {
             up = false;
         }
+    }
 
+
+    private void descend(){
         if(!up && height-100 > dudeY) {
             dudeY+=4;
         }
@@ -98,17 +84,4 @@ public class GeometryDash extends Application {
         }
     }
 
-    public void newBlock() {
-
-        Timeline blockMovement = new Timeline(new KeyFrame(Duration.seconds(.01), new EventHandler<ActionEvent>() {
-
-            double i = 1000 ;
-            public void handle(ActionEvent event) {
-                r1.setX(i);
-                i--;
-            }
-        }));
-        blockMovement.setCycleCount(Timeline.INDEFINITE);
-        blockMovement.play();
-    }
 }
